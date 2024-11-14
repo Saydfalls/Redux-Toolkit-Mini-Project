df022a7ee10a7d772e48ed48e4533027f4f278db
Adrian Rodriguez-Cruz <airodcruz2001@gmail.com>
2024-11-14 15:52:03 -0500

    feat: add Counter.jsx functionality
    Added the necessary functionality to make counter increment and decrement using the redux toolkit. This meant adding the useSelector and useDispatch functions and their respective functionality and files.It also meant fleshing out the functions that handle interactions with the increment/decrement buttons.

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../redux/actions/counterAction";
 
 const Counter = () => {
-  // Initialize the `counter` value to `0`.
-  const counter = 0;
+  const counter = useSelector((state => state.counter)); // useSelector accesses counter state from the redux store
+  const dispatch = useDispatch(); // useDispatch accesses Redux's dispatch function
 
   // Define a function to handle incrementing the `counter`.
   const handleIncrement = () => {
     // Implement the logic for incrementing the `counter` here.
+    dispatch(increment());
   };
 
   // Define a function to handle decrementing the `counter`.
   const handleDecrement = () => {
     // Implement the logic for decrementing the `counter` here.
+    dispatch(decrement());
   };
 
   return (
