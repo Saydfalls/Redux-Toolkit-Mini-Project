abde4caed9410a51853f8c3ef7e67158accae668
Adrian Rodriguez-Cruz <airodcruz2001@gmail.com>
2024-11-14 15:06:31 -0500

    docs: add comments in store.js for better understanding

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./reducers/counterReducer";
 
 const store = configureStore({
+    // Specify reducers for each state slice
     reducer: {
+        // 'counterReducer' handles state change
         counter: counterReducer,
     },
 });
