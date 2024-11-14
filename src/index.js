60041d611928001900e9fe64fad36afa0e24c378
Adrian Rodriguez-Cruz <airodcruz2001@gmail.com>
2024-11-14 15:10:18 -0500

    docs: add comments in index.js for better understanding

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App/App";
-import store from "./redux/store";
+import store from "./redux/store"; // Import redux store
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  // The `<Provider>` component allows components within its tree to access the Redux `store`.
   <Provider store={store}>
     <React.StrictMode>
       <App />
