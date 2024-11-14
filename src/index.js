1755e6f3d4f2b457e8b6a2a31a83d434ed05f136
Adrian Rodriguez-Cruz <airodcruz2001@gmail.com>
2024-11-14 14:00:31 -0500

    chore: restart, too many issues with original project set up
    The original project was set up quite poorly for whatever reason so I decided to delete it and restart it with the redux template in place from the start.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App/App';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-import { BrowserRouter } from 'react-router-dom';
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
