be89d7646ffac478d2eeba0ccb8cdbcd9bd7fbc8
Adrian Rodriguez-Cruz <airodcruz2001@gmail.com>
2024-11-14 15:17:12 -0500

    docs: add comments in counterAction.js for better understanding

--- src/redux/actions/counterAction.js
+++ src/redux/actions/counterAction.js
@@ -4,3 +4,10 @@ import { createAction } from "@reduxjs/toolkit";
 // Creating action creator functions for incrementing and decrementing the counter
 export const increment = createAction('counter/increment');
 export const decrement = createAction('counter/decrement');
+
+/* 
+    The strings represent action types.
+    It is common to prefix the action type with the feature or slice name for better organization.
+    (In this case it's counter)
+    Using string constants or variables is recommended for consistency and easier refactoring.
+*/
